83467b725fc7f6c6a90763b8e2594dc0e4dc5060
kdy0618 <kdy06187@gmail.com>
2025-02-28 00:04:45 +0900

    d
    d

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,8 +7,12 @@ public class GameManager : MonoBehaviour
     void Start()
     {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         
+=======
+        instance = this;
+>>>>>>> parent of 8afa480 (123123)
     }
 >>>>>>> parent of 906822a (h)
 
