6d9aaaf5aa227a25f7f706f0484f5d5e4521defa
kdy0618 <kdy06187@gmail.com>
2025-02-28 00:03:59 +0900

    d
    d

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,6 +6,11 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+<<<<<<< HEAD
+=======
+        
+    }
+>>>>>>> parent of 906822a (h)
 
         instance = this; //fucking  singleton
 
