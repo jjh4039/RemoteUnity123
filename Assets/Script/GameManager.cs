e5f6572e7fee00debb508ec27b5ec3a255d50d68
정재하 <jjh4039@naver.com>
2025-02-28 00:18:07 +0900

    버그 수정

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,19 +6,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        
-=======
         instance = this;
->>>>>>> parent of 8afa480 (123123)
-    }
->>>>>>> parent of 906822a (h)
-
-        instance = this; //fucking  singleton
-
-
    }
 
     void Update()
