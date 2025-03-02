e102dd29733649328c0d933389ca238c34d83862
kdy0618 <kdy06187@gmail.com>
2025-03-02 21:44:46 +0900

    스폰

--- Assets/Script/CheckPoint.cs
+++ Assets/Script/CheckPoint.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
@@ -5,21 +6,28 @@ public class CheckPoint : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public bool checkPointReached = false;
     public BoxCollider2D Colider;
+    public Vector3 SpawnPoint;
+    public GameObject Check;
+    public Spawner spawner;
+
     void Start()
     {
         Colider = GetComponent<BoxCollider2D>();
+        spawner = GetComponent<Spawner>();
+        Check = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(checkPointReached);
+ 
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             checkPointReached = true;
+            spawner.SpawnPoint = transform.position;
         }
         
     }
