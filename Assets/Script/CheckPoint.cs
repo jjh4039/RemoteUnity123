9d2290392600e7d8db6f4c620bf982c74279d61b
kdy0618 <kdy06187@gmail.com>
2025-03-05 19:33:14 +0900

    체크포인트 애니메이션
    ㅗ

--- Assets/Script/CheckPoint.cs
+++ Assets/Script/CheckPoint.cs
@@ -1,4 +1,5 @@
 using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
@@ -8,36 +9,55 @@ public class CheckPoint : MonoBehaviour
     public BoxCollider2D Colider;
     public GameObject Check;
     public GameObject gameobject;
-
+    public Animator animator;
 
     void Start()
     {
         Colider = GetComponent<BoxCollider2D>();
-
+        animator = GetComponent<Animator>();
         Check = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
- 
+        if(SpawnManager.instance.SpawnPoint == transform.position)
+        {
+            checkPointReached = true;
+        }
+        else
+        {
+            checkPointReached = false;
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !checkPointReached)
         {
             checkPointReached = true;
+            StartCoroutine(FlagOut());
             SpawnManager.instance.SpawnPoint = transform.position;
 
         }
         
     }
+    /*
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             checkPointReached = false;
+
         }
+    }
+    */
+    IEnumerator FlagOut()
+    {
+        animator.SetBool("Flag Idle", false);
+        animator.SetBool("Flag Out", true);
+        yield return new WaitForSeconds(2f);
+        animator.SetBool("Flag Out", false);
+        animator.SetBool("Flag Idle", true);
     }
 }
 
