36b3a9b7b8d8845aca0b310ebb233b3a4069d703
정재하 <jjh4039@naver.com>
2025-05-04 14:59:39 +0900

    5.4 1차
    컷신 스킵 Text

--- Assets/Script/CutScene.cs
+++ Assets/Script/CutScene.cs
@@ -6,17 +6,22 @@ public class CutScene : MonoBehaviour
 {
     public Image[] CutSceneBox;
     public CanvasGroup pade;
+    public GameObject skipText;
 
+    // 컷신 레터박스 & 자막
     public IEnumerator CutSceneStart()
     {
-        for (int i = 0; i < 100; i++) // 컷신박스 
+        for (int i = 0; i < 100; i++) 
         {
             CutSceneBox[0].rectTransform.localPosition = new Vector3(0, CutSceneBox[0].rectTransform.localPosition.y - 1, 0);
             CutSceneBox[1].rectTransform.localPosition = new Vector3(0, CutSceneBox[1].rectTransform.localPosition.y + 1, 0);
             yield return new WaitForSeconds(0.01f);
         }
+        yield return new WaitForSeconds(0.3f);
+        skipText.SetActive(true);
     }
 
+    // 페이드 아웃
     public IEnumerator PadeOut()
     {
         for(int i = 0; i < 100; i++)
