a8c446c385c332844685fd3c858e9bb857f67489
정재하 <jjh4039@naver.com>
2025-05-20 00:25:16 +0900

    5.20
    1. StageTitle Implement

--- Assets/Script/Village/StageManager.cs
+++ Assets/Script/Village/StageManager.cs
@@ -10,16 +10,21 @@ public class StageManager : MonoBehaviour
         Stage1
     }
 
+    [Header("Components")]
+    public CanvasGroup StageTitle;
+
+
     [Header("Field")]
     public StageName stageID;
 
     void Start()
     {
         GameManager.Instance.cutScene.StartCoroutine("PadeIn");
+        StageTitle.alpha = 0f;
         switch (stageID)
         {
             case StageName.SeedVillage:
-               
+                StartCoroutine(TitleOn(StageName.SeedVillage));
                 break;
             case StageName.Stage1:
 
@@ -29,8 +34,23 @@ public class StageManager : MonoBehaviour
         }
     }
 
-    void Update()
+    public IEnumerator TitleOn(StageName ST)
     {
-        
-    }
+        yield return new WaitForSeconds(0.3f);
+
+        for (int i = 0; i < 100; i++)
+        {
+            StageTitle.alpha += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        yield return new WaitForSeconds(3f);
+
+
+        for (int i = 0; i < 100; i++)
+        {
+            StageTitle.alpha -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        }
+    } 
 }
